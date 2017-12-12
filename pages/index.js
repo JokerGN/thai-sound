16ec21a3b5acf318692bcd20ba95a5ee0f7a9fc0
JokerGN <chote1234@hotmail.co.th>
2017-12-12 17:08:58 +0700

    {WIP} Add sidenav

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import NavBar from '../components/NavBar'
+import SideNav from '../components/SideNav'
+import {Grid, Col, Row} from 'react-styled-flexboxgrid'
 
 export default () => (
   <div>
     <NavBar />
+    <Grid>
+    <Row>
+      <Col xs={3} md={3}><SideNav /></Col>
+      <Col xs={6} md={9}><NavBar /></Col>
+    </Row>
+  </Grid>
   </div>
 )
