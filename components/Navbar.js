003db9eccf45eb3745b21ea9db81f9911c35e76f
JokerGN <chote1234@hotmail.co.th>
2017-12-13 23:20:11 +0700

    Add structure design for dashboard page

---
+++ components/Navbar.js
@@ -0,0 +1,173 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import { MenuList, MenuItem } from 'material-ui/Menu'
+import { ListItemIcon, ListItemText } from 'material-ui/List'
+import Drawer from 'material-ui/Drawer'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import List from 'material-ui/List'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import Hidden from 'material-ui/Hidden'
+import Divider from 'material-ui/Divider'
+import MenuIcon from 'material-ui-icons/Menu'
+import InboxIcon from 'material-ui-icons/MoveToInbox'
+import DraftsIcon from 'material-ui-icons/Drafts'
+import SendIcon from 'material-ui-icons/Send'
+import Button from 'material-ui/Button'
+
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  flex: {
+    flex: 1,
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 250,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+  menuList: {
+    height: '100%'
+  },
+})
+
+class Navbar extends React.Component {
+  state = {
+    mobileOpen: false
+  }
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen })
+  }
+
+  render() {
+    const { classes, theme } = this.props
+
+    const drawer = (
+      <div>
+        <div className={classes.drawerHeader} />
+        <MenuList>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Sent mail" />
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Drafts" />
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Inbox" />
+        </MenuItem>
+      </MenuList>
+      </div>
+    )
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="contrast"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography type="title" color="inherit" noWrap className={classes.flex}>
+                Thai-sound
+              </Typography>
+              <Button color="contrast">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp>
+            <Drawer
+              type="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              onRequestClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden mdDown implementation="css">
+            <Drawer
+              type="permanent"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <main className={classes.content}>
+            <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+          </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles, { withTheme: true })(Navbar)
