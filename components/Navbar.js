abefa4a7707bbff2b2097a81a105e825883d3d12
JokerGN <chote1234@hotmail.co.th>
2017-12-15 00:01:08 +0700

    {WIP}

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
 import MenuIcon from 'material-ui-icons/Menu'
+import ChevronLeftIcon from 'material-ui-icons/ChevronLeft'
 import Button from 'material-ui/Button'
+import Drawer from 'material-ui/Drawer'
 
-const styles = {
+const drawerWidth = 240
+
+const styles = theme => ({
   root: {
-    width: '100%'
+    width: '100%',
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden'
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%'
+  },
+  appBar: {
+    position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
   },
   flex: {
     flex: 1
@@ -18,29 +58,73 @@ const styles = {
   menuButton: {
     marginLeft: -12,
     marginRight: 20
+  },
+})
+
+class Navbar extends React.Component {
+
+  state = {
+    open: false
+  }
+
+  handleDrawerOpen = () => (
+    this.setState({ open: true })
+  )
+
+  handleDrawerClose = () => (
+    this.setState({ open: false })
+  )
+
+  render () {
+    const { classes, theme } = this.props
+    const { open } = this.state
+    const drawer = (
+      <Drawer
+        type='persistent'
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        open={open}
+      >
+        <div className={classes.drawerInner}>
+          <div>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+        </div>
+      </Drawer>
+    )
+
+    return (
+      <div className={classes.root}>
+      <div className={classes.appFrame}>
+        <AppBar className={classNames(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color='contrast'
+              aria-label="open menu"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography type='title' color='inherit' className={classes.flex}>Thai-sound</Typography>
+            <Button color="contrast">เข้าสู่ระบบ</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      </div>
+    )
   }
 }
 
-const Navbar = (props) => {
-  const { classes } = props
-
-  return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton color='contrast' aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type='title' color='inherit' className={classes.flex}>Thai-sound</Typography>
-          <Button color="contrast">เข้าสู่ระบบ</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
-}
-
 Navbar.PropTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(Navbar)
