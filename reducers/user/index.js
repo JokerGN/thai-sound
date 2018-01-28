0bd0e5f48fcfe35d9d18786fa02d9042326ffa10
JokerGN <chote1234@hotmail.co.th>
2018-01-28 15:13:21 +0700

    Fix format of user data

--- reducers/user/index.js
+++ reducers/user/index.js
@@ -4,7 +4,11 @@ import * as types from '../../constants/actionTypes'
 export default function (state = initialState.user, action) {
   switch (action.type) {
     case types.LOGIN_SUCCESS:
-      return {...state, data: action.user}
+      return {...state, data: {
+        status: action.user[0].status,
+        name: action.user[0].firstName,
+        role: action.user[0].role
+      }}
     case types.LOGIN_ERROR:
       return {...state, data: {
         status: action.error.response.status,
